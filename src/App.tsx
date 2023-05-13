b95b46c4f9665f63ca3324d2b96501e822124323
WaerXD <waermod123@gmail.com>
2023-05-13 05:29:53 +0300

    done with app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoList from "./components/ToDoList";
-import ControlPanel from "./components/ControlPanel";
-import { Col, Row } from "antd";
+import { Col, Row, Form, Button, Input, Space } from "antd";
 import { useTodos } from "./hooks/todos";
+import axios from "axios";
+import IToDo from "./interfaces/IToDo";
+
+const URL = "http://localhost:3100/todo";
 
 function App() {
-  const { todoList } = useTodos();
+  const [form] = Form.useForm();
+  const { todoList, reloadTodo } = useTodos();
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputDescription, setInputDescription] = useState("");
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const handleCreate = async (todo: IToDo) => {
+    await axios.post(URL, {
+      title: inputTitle,
+      description: inputDescription,
+      isCompleted: false,
+    });
+    reloadTodo();
+    onReset();
+  };
+
+  const deleteAllTodos = async () => {
+    await axios.delete(URL);
+    reloadTodo();
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  const onReload = () => {
+    reloadTodo();
+  }
+
   return (
     <>
       <h1>ToDo App</h1>
       <hr />
       <Row>
-        <Col span={8}>
+        <Col span={8} >
+          {}
           {todoList.map((todo) => (
-            <ToDoList todo={todo} key={todo.id} />
+            <ToDoList todo={todo} key={todo.id} onReload={onReload}/>
           ))}
         </Col>
         <Col span={8}>
-          <ControlPanel />
+          <Space direction="vertical" style={{ marginTop: 30 }}>
+            <Button
+              type="primary"
+              style={{ width: 200, background: "green" }}
+              onClick={() => {
+                reloadTodo();
+              }}
+            >
+              Обновить
+            </Button>
+            <Button
+              type="primary"
+              danger
+              style={{ width: 200 }}
+              onClick={() => {
+                deleteAllTodos();
+              }}
+            >
+              Удалить всё
+            </Button>
+          </Space>
         </Col>
-        <Col span={8}></Col>
+        <Col span={8}>
+          <Form
+            form={form}
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600, marginTop: 30, marginRight: 70 }}
+            initialValues={{ remember: true }}
+            onFinish={handleCreate}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Заголовок"
+              name="title"
+              initialValue={""}
+              rules={[
+                { required: true, message: "Пожалуйста введите заголовок" },
+              ]}
+            >
+              <Input
+                value={inputTitle}
+                onChange={(event) => {
+                  setInputTitle(event.target.value);
+                }}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Описание"
+              name="description"
+              initialValue={""}
+              rules={[
+                { required: true, message: "Пожалуйста введите описание" },
+              ]}
+              style={{ marginTop: 10 }}
+            >
+              <Input
+                value={inputDescription}
+                onChange={(event) => {
+                  setInputDescription(event.target.value);
+                }}
+              />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ marginRight: 10, width: 200, background: "#e3dd22" }}
+              >
+                Создать
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
       </Row>
     </>
   );
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,58 @@
-import React from "react";
-import { Card, Space } from "antd";
+import React, { useState } from "react";
+import { Card, Space, Modal, Input, Form, Button } from "antd";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import IToDo from "../interfaces/IToDo";
 import axios from "axios";
+
 const URL = "http://localhost:3100/todo";
 
 interface ToDo {
   todo: IToDo;
+  onReload: () => void;
 }
 
-async function deleteTodo(id: number | undefined) {
-  await axios.delete(URL + "/" + id);
-}
+function ToDoList({todo, onReload}: ToDo) {
+  const [titleValue, setTitleValue] = useState("");
+  const [descriptionValue, setDescriptionValue] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-function ToDoList({ todo }: ToDo) {
+  const handleUpdateTodos = async () => {
+    await axios.patch(URL + "/" + todo.id, {
+      title: titleValue,
+      description: descriptionValue,
+    });
+    onReload();
+    handleCancel();
+    resetInputs();
+  };
+
+  const deleteTodo = async () => {
+    await axios.delete(URL + "/" + todo.id);
+    onReload();
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleUpdateValues = () => {
+    setTitleValue(todo.title);
+    setDescriptionValue(todo.description);
+  };
+
+  const resetInputs = () => {
+    setTitleValue("");
+    setDescriptionValue("");
+  };
+
   return (
     <Space direction="vertical" size={16}>
       {" "}
@@ -21,17 +60,82 @@ function ToDoList({ todo }: ToDo) {
         title={todo.title}
         style={{ width: 300, border: "solid black 1px" }}
         actions={[
-          <EditOutlined key="edit" />,
+          <EditOutlined
+            key="edit"
+            onClick={() => {
+              showModal();
+              handleUpdateValues();
+            }}
+          />,
           <DeleteOutlined
             key="delete"
             onClick={() => {
-              deleteTodo(todo.id);
+              deleteTodo();
             }}
           />,
         ]}
       >
         <p>{todo.description}</p>
       </Card>
+      <Modal
+        title="Изменить"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        {" "}
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          style={{ maxWidth: 600, marginTop: 20, marginRight: 70 }}
+          initialValues={{ remember: true }}
+          onFinish={handleUpdateTodos}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Заголовок"
+            name="title"
+            initialValue={titleValue}
+            rules={[
+              { required: true, message: "Пожалуйста введите заголовок" },
+            ]}
+          >
+            <Input
+              value={titleValue}
+              onChange={(event) => {
+                setTitleValue(event.target.value);
+              }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Описание"
+            name="description"
+            initialValue={descriptionValue}
+            rules={[{ required: true, message: "Пожалуйста введите описание" }]}
+            style={{ marginTop: 10 }}
+          >
+            <Input
+              value={descriptionValue}
+              onChange={(event) => {
+                setDescriptionValue(event.target.value);
+              }}
+            />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ marginRight: 60 }}
+            >
+              Сохранить
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </Space>
   );
 }
